436eef1b77fb5008b99ccd483123d405b8130d69
praveen <praveenkumar_govindaraj@comcast.com>
2025-02-05 16:10:20 +0530

    secound commit done

--- Navi/Navi/AppDelegate.swift
+++ Navi/Navi/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         print("first commit")
+        print("2 commit")
+
         return true
     }
 
